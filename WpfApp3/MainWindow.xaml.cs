c02875b5ed0dcefd963f190c9bf8718bf0253a37
Gabriel Martinez <n/a>
2017-09-11 00:44:59 -0700

    Add project files.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{//Gabe
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+             
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string text = this.textBox.Text;
+            this.testEscapeKeys(text);
+        }
+        public void testEscapeKeys(string text)
+        {
+            var regexItem = new Regex("^[a-zA-Z0-9 ]*$");
+
+            if (regexItem.IsMatch(text))
+            {
+       
+                this.textBox.Text = "";
+                throw new Exception("Escape keys found");
+            }
+
+        }
+    }
+}
---
+++ WpfApp3Tests/MainWindowTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfApp3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3.Tests
+{
+    [TestClass()]
+    public class MainWindowTests
+    {
+        [TestMethod()]
+        public void testEscapeKeysTest()
+        {
+            MainWindow mw = new MainWindow();
+            try
+            {
+                mw.testEscapeKeys("testing");
+            }catch(Exception e)
+            {
+                StringAssert.Contains(e.Message, "Escape keys found");
+
+                return;
+            }
+                Assert.Fail("Nothing wrong");
+        }
+    }
+}
